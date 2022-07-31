7fccad2cbb6b8a759d42e7e586b3a2ede07399df
dalienst <oduordalienst@gmail.com>
2022-07-31 13:01:38 +0300

    Learning React

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+const Contact = () => {
+    const myStyle = {
+        color: "white",
+        backgroundColor: "DodgerBlue",
+        padding: "10px",
+        fontFamily: "Sans-Serif"
+    };
+    return (
+        <>
+          <h1 style={myStyle}>Hello Style!</h1>
+          <p>Add a little style!</p>
+        </>
+      );
+  };
+  
+  export default Contact;
